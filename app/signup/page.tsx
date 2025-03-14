1b5ce2b378d8da5eb6b3c4e0682cab7377224c08
c137-pickle-rick1 <c137_pickle_rick1@proton.me>
2025-03-14 15:14:22 +0800

    fix type error

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,17 @@
 import { SignupForm } from "@/components/signup-form"
 
-export default function SignupPage({searchParams}: {searchParams: {code?: string}}) {
+export default async function SignupPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const params = await searchParams;
+  const code = typeof params.code === 'string' ? params.code : undefined;
+
   return (
     <div className="w-full min-h-[calc(100vh-134px)] bg-slate-50 flex flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <SignupForm code={searchParams.code} />
+        <SignupForm code={code} />
       </div>
     </div>
   )
