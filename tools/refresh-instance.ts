5be4d449036e54042e1e78e20535ce10bc3c3611
陆凌风 <lulingfengweb@gmail.com>
2025-03-31 16:12:48 +0800

    fix: 刷新token兼容性处理

--- tools/refresh-instance.ts
+++ tools/refresh-instance.ts
@@ -1,8 +1,27 @@
 import { RefreshToken } from './refresh-token';
 import { logout } from './auth';
+import axios from 'axios';
 export const refreshInstance = new RefreshToken({
   refreshUrl: `${process.env.NEXT_PUBLIC_API_URL}/user/refresh-token`,
   cookiePrefix: '',
+  fetchFunction: async (url: string, token: string) => {
+    const res = await axios.post(
+      url,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (res.status !== 200) {
+      throw new Error('refresh token failed');
+    }
+    if (res.data.code !== 200) {
+      throw new Error(res.data.message);
+    }
+    return res.data.data;
+  },
   onLogout: () => {
     logout();
   },
