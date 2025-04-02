8440ecb4eeba7862c5ec82f0ee6dd85e76cfba95
陆凌风 <lulingfengweb@gmail.com>
2025-04-02 15:50:52 +0800

    dev: 套餐模块开发

--- components/sections/price-section.tsx
+++ components/sections/price-section.tsx
@@ -4,111 +4,55 @@ import { Separator } from "@/components/ui/separator"
 import { Section, SectionHeader, SectionTitle, SectionDescription } from "@/components/sections/section"
 import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger, } from "@/components/ui/tabs"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 import { Button } from "../ui/button"
 import { CheckCircle } from "@phosphor-icons/react"
 import { getPackages } from "@/api/packages"
 
-type BillingCycle = 'monthly' | 'quarterly' | 'halfYearly' | 'yearly'
+type BillingCycle = 'monthly' | 'quarterly' | 'semi_annually' | 'annually'
 
-interface BillingCycleInfo {
+interface BillingCycleKey {
 	label: string
-	discount: number
+	valueKey: keyof Packages.Item
+	discountKey: keyof Packages.SiteConfig | ''
 }
-
-const BILLING_CYCLES: Record<BillingCycle, BillingCycleInfo> = {
+const BILLING_CYCLES: Record<BillingCycle, BillingCycleKey> = {
 	monthly: {
 		label: '月付',
-		discount: 1
+		valueKey: 'monthly_price',
+		discountKey: '',
 	},
 	quarterly: {
 		label: '季付',
-		discount: 0.88
+		valueKey: 'quarterly_price',
+		discountKey: 'quarterly_discount',
 	},
-	halfYearly: {
+	semi_annually: {
 		label: '半年付',
-		discount: 0.75
+		valueKey: 'semi_annually_price',
+		discountKey: 'semi_annually_discount',
 	},
-	yearly: {
+	annually: {
 		label: '年付',
-		discount: 0.66
+		valueKey: 'annually_price',
+		discountKey: 'annually_discount',
 	}
 }
-
-const PRICE_DATA = {
-	'free': {
-		name: '免费套餐',
-		description: '每日免费使用1小时（线路不限）',
-		monthlyPrice: 0,
-		features: {
-			base: [
-				'最多3台设备同时在线',
-				'不限制峰值速率',
-				'轻舟自研翻墙协议',
-			],
-			route: [
-				'基础线路（每日总计限免1小时）',
-				'CN2线路（每日总计限免1小时）',
-			],
-			others: [
-
-			]
-		}
-	},
-	'basic': {
-		name: '基础套餐',
-		description: '基础线路，CN2线路（每日限免1小时）',
-		monthlyPrice: 3,
-		features: {
-			base: [
-				'最多3台设备同时在线',
-				'不限制峰值速率',
-				'轻舟自研翻墙协议',
-			],
-			route: [
-				'基础线路',
-				'CN2线路（每日限免1小时）',
-			],
-			others: [
-
-			]
-		}
-	},
-	'pro': {
-		name: '高级套餐',
-		description: '基础线路，CN2线路',
-		monthlyPrice: 5,
-		features: {
-			base: [
-				'最多3台设备同时在线',
-				'不限制峰值速率',
-				'轻舟自研翻墙协议',
-			],
-			route: [
-				'基础线路',
-				'CN2线路',
-			],
-			others: [
-				'技术支持优先响应',
-			]
-		}
-	}
-}
-
 export function PriceSection() {
-	const [billingCycle, setBillingCycle] = useState<BillingCycle>('yearly')
-	const [packages, setPackages] = useState<Packages.Res>([])
+	const [billingCycle, setBillingCycle] = useState<BillingCycle>('monthly')
+	const [packages, setPackages] = useState<Packages.Item[]>([])
+	const [siteConfig, setSiteConfig] = useState<Packages.SiteConfig | null>(null)
 	useEffect(() => {
 		const fetchData = async () => {
-			const res = await getPackages()
-			console.log('res', res)
-			setPackages(res.data)
+			const { data } = await getPackages()
+			setPackages(data.packages)
+			setSiteConfig(data.site_config)
 		}
 		fetchData()
 	}, [])
 	useEffect(() => {
-		console.log('packages', packages)
-	}, [packages])
+		console.log('packages', packages, siteConfig)
+	}, [packages, siteConfig])
 
 	return (
 		<Section id="choose-plan">
@@ -128,39 +72,23 @@ export function PriceSection() {
 					))}
 				</TabsList>
 				<TabsContent value="monthly" className="w-full mt-6 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-					{Object.entries(PRICE_DATA).map(([key, plan]) => (
-						<PriceCard
-							key={key}
-							plan={plan}
-							billingCycle={billingCycle}
-						/>
+					{packages.map((plan) => (
+						<PriceCardData key={plan.id} plan={plan} tabKey={BILLING_CYCLES[billingCycle]} siteConfig={siteConfig} />
 					))}
 				</TabsContent>
 				<TabsContent value="quarterly" className="w-full mt-6 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-					{Object.entries(PRICE_DATA).map(([key, plan]) => (
-						<PriceCard
-							key={key}
-							plan={plan}
-							billingCycle={billingCycle}
-						/>
+					{packages.map((plan) => (
+						<PriceCardData key={plan.id} plan={plan} tabKey={BILLING_CYCLES[billingCycle]} siteConfig={siteConfig} />
 					))}
 				</TabsContent>
-				<TabsContent value="halfYearly" className="w-full mt-6 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-					{Object.entries(PRICE_DATA).map(([key, plan]) => (
-						<PriceCard
-							key={key}
-							plan={plan}
-							billingCycle={billingCycle}
-						/>
+				<TabsContent value="semi_annually" className="w-full mt-6 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+					{packages.map((plan) => (
+						<PriceCardData key={plan.id} plan={plan} tabKey={BILLING_CYCLES[billingCycle]} siteConfig={siteConfig} />
 					))}
 				</TabsContent>
-				<TabsContent value="yearly" className="w-full mt-6 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-					{Object.entries(PRICE_DATA).map(([key, plan]) => (
-						<PriceCard
-							key={key}
-							plan={plan}
-							billingCycle={billingCycle}
-						/>
+				<TabsContent value="annually" className="w-full mt-6 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+					{packages.map((plan) => (
+						<PriceCardData key={plan.id} plan={plan} tabKey={BILLING_CYCLES[billingCycle]} siteConfig={siteConfig} />
 					))}
 				</TabsContent>
 			</Tabs>
@@ -248,4 +176,125 @@ function PriceCard({ plan, billingCycle }: PriceCardProps) {
 			</CardFooter>
 		</Card>
 	)
-} 
+}
+
+
+function PriceCardData({ plan, tabKey, siteConfig }: { plan: Packages.Item, tabKey: BillingCycleKey, siteConfig: Packages.SiteConfig | null }) {
+	const price = useMemo(() => {
+		return plan[tabKey.valueKey] as string
+	}, [plan, tabKey])
+
+	useEffect(() => {
+		console.log('tabKey', tabKey, siteConfig)
+	}, [tabKey, siteConfig])
+
+	// 单位 0对应天 1对应周 2对应月
+	const getUnit = (unit: 0 | 1 | 2) => {
+		let str = ''
+		switch (unit) {
+			case 0:
+				str = '天'
+				break
+			case 1:
+				str = '周'
+				break
+			case 2:
+				str = '月'
+				break
+		}
+		return str
+	}
+	return (
+		<Card>
+			<CardContent className="flex flex-col items-start gap-3">
+				<div className="flex flex-col items-start gap-1">
+					<CardTitle className="text-xl">{plan.name}</CardTitle>
+					<CardDescription className="text-sm">{plan.description}</CardDescription>
+				</div>
+
+				<div className="flex items-baseline gap-1">
+					<span className="text-2xl">￡</span>
+					<span className="text-[2.5rem] font-medium">{price}</span>
+					<span className="text-1xl text-muted-foreground">/ 月</span>
+					{tabKey.discountKey && siteConfig && (
+						<span className="text-xs bg-rose-400 text-white rounded px-[5px] py-[2px]">节约{((1 - siteConfig[tabKey.discountKey]) * 100).toFixed(0)}%</span>
+					)}
+				</div>
+
+				<Button className="w-full" size="lg" onClick={() => plan.user_subscribed ? document.getElementById('download')?.scrollIntoView({ behavior: 'smooth' }) : null}>
+					{plan.user_subscribed ? '立即下载' : '立即订阅'}
+				</Button>
+			</CardContent>
+
+			<Separator />
+
+			<CardFooter>
+				<div className="w-full flex flex-col gap-4">
+					<div className="flex flex-col gap-2">
+						<div className="text-xs text-muted-foreground">基础权益</div>
+						<ul className="flex flex-col gap-1">
+							<li className="flex items-center gap-1 m-0">
+								<CheckCircle size={20} className="text-green-500" weight="fill" />
+								最多{plan.max_online_devices}台设备同时在线
+							</li>
+							<li className="flex items-center gap-1 m-0">
+								<CheckCircle size={20} className="text-green-500" weight="fill" />
+								不限制峰值速率
+							</li>
+							<li className="flex items-center gap-1 m-0">
+								<CheckCircle size={20} className="text-green-500" weight="fill" />
+								轻舟自研翻墙协议
+							</li>
+						</ul>
+					</div>
+					<div className="flex flex-col gap-2">
+						<div className="text-xs text-muted-foreground">
+							线路
+							{
+								plan.time_limit || plan.traffic_limit ? (
+									<span className="text-xs">
+										（整体限制 &nbsp;
+										{plan.time_limit && `每${getUnit(plan.time_period)}${plan.time_limit}小时，`}
+										{plan.traffic_limit && `每${getUnit(plan.traffic_period)}${plan.traffic_limit}GB流量`}
+										）
+									</span>
+								) : null
+							}
+						</div>
+						<ul className="flex flex-col gap-1">
+							{plan.package_node_groups.map((group) => (
+								<li key={group.id} className="flex items-center gap-1 m-0">
+									<CheckCircle size={20} className="text-green-500" weight="fill" />
+									{group.node_group.name}线路
+									{
+										group.time_limit && (
+											<span className="text-xs text-muted-foreground">（每{getUnit(group.time_period)}{group.time_limit}小时）</span>
+										)
+									}
+									{
+										group.traffic_limit && (
+											<span className="text-xs text-muted-foreground">（每{getUnit(group.traffic_period)}{group.traffic_limit}GB流量）</span>
+										)
+									}
+								</li>
+							))}
+						</ul>
+					</div>
+					{/* {plan.features.others.length > 0 && (
+						<div className="flex flex-col gap-2">
+							<div className="text-xs text-muted-foreground">其他</div>
+							<ul className="flex flex-col gap-1">
+								{plan.features.others.map((feature, index) => (
+									<li key={index} className="flex items-center gap-1 m-0">
+										<CheckCircle size={20} className="text-green-500" weight="fill" />
+										{feature}
+									</li>
+								))}
+							</ul>
+						</div>
+					)} */}
+				</div>
+			</CardFooter>
+		</Card>
+	)
+}
--- types/packages.d.ts
+++ types/packages.d.ts
@@ -1,4 +1,44 @@
 declare namespace Packages {
   // type Params = FormData;
-  type Res = Response<null>;
+  interface NodeGroup {
+    id: number;
+    package_id: number;
+    node_group_id: number;
+    is_time_limited: number;
+    is_traffic_limited: number;
+    traffic_limit: number;
+    traffic_period: 0 | 1 | 2; // 0对应天 1对应周 2对应月
+    time_limit: number;
+    time_period: 0 | 1 | 2; // 0对应天 1对应周 2对应月
+    node_group: {
+      id: number;
+      name: string;
+      nodes: any[];
+    };
+  }
+  interface Item {
+    id: number;
+    name: string;
+    description: string;
+    monthly_price: string;
+    quarterly_price: string;
+    semi_annually_price: string;
+    annually_price: string;
+    time_limit: number;
+    time_period: 0 | 1 | 2; // 0对应天 1对应周 2对应月
+    traffic_limit: number;
+    traffic_period: 0 | 1 | 2; // 0对应天 1对应周 2对应月
+    max_online_devices: number;
+    package_node_groups: NodeGroup[];
+    user_subscribed: boolean;
+  }
+  interface SiteConfig {
+    annually_discount: number;
+    quarterly_discount: number;
+    semi_annually_discount: number;
+  }
+  type Res = Response<{
+    packages: Item[];
+    site_config: SiteConfig;
+  }>;
 }
