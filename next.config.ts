18eea6510dce1c4614767c1db163a5e8430ee53e
陆凌风 <lulingfengweb@gmail.com>
2025-04-03 19:05:24 +0800

    fix: 图片渲染报错

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // reactStrictMode:false,
   images: {
-    domains: ['192.168.0.106'],
+    domains: ['qz-api.testlab.fun'],
     remotePatterns: [
       {
         protocol: 'https',
